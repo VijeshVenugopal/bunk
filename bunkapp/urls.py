a4620491a98fcc0476634a09fe498d2e7357d169
Febry <febry@febry-Satellite-C640.(none)>
2016-01-24 18:23:05 +0530

    dashboard contents added

--- bunkapp/urls.py
+++ bunkapp/urls.py
@@ -3,6 +3,6 @@ from bunkapp.views import *
 
 urlpatterns = [
 	#url(r'/$',LandingPage.as_view(),name='landing_page'),
-    url(r'$',LandingView.as_view()),
-    url(r'dashboard/',dashboard,name='login'),
+    url(r'^$',LandingView.as_view()),
+    url(r'^dashboard/$',dashboard,name='login'),
 ]
