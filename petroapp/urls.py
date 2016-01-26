adfa7087be003b8a45384582faf60e421e951508
vijeshv <vijeshtv1988@gmail.com>
2016-01-26 21:07:33 +0530

    user entry section

---
+++ petroapp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from petroapp.views import *
+
+urlpatterns = [
+    url(r'^entries/$', EntryListView.as_view(), name="entry-list"),
+    url(r'^(?P<username>\w+)/$',UserEntryView.as_view(), name="user_entry"),
+
+]
