d8e70a6ac9192d28add766c12258f0e3b2f4c613
vijeshv <vijeshtv1988@gmail.com>
2016-01-28 22:40:36 +0530

    changes(vijesh) added

--- petroapp/views.py
+++ petroapp/views.py
@@ -4,6 +4,8 @@ from django.views.generic import View
 from django.views.generic import CreateView, ListView
 from django.http import HttpResponse
 from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from petroapp.forms import *
 
@@ -14,23 +16,26 @@ class EntryListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(EntryListView, self).get_context_data(**kwargs)
 	context['entries'] = DailyInputs.objects.filter(date__startswith=date.today())	
-	print context
 	return context
 
 
-class UserEntryView(CreateView):
-    model = DailyInputs
-    form_class = EmployeeEntryForm
-    template_name = "employee/employee_entry.html"     
+class UserEntryView(View):
+    template_name = "employee/employee_entry.html"   
 
-    def form_valid(self, form):
-	entry = form.save(commit=False)
-	entry.user = self.request.user
-	entry.save()
-	messages.success(self.request, "entry added successfully")
-        return render(self.request,self.template_name,{'form':EmployeeEntryForm()})
+    def get(self, request, *args, **kwargs):
+        form = EmployeeEntryForm()
+	context = {}
+	context['form'] = form
+	return render(self.request,self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+	form = EmployeeEntryForm(request.POST)
+	if form.is_valid():
+	    entry = form.save(commit=False)
+	    entry.user = self.request.user
+            entry.save()
+	    messages.success(self.request, "entry added successfully")
+	    entries = DailyInputs.objects.filter(date__startswith=date.today())	
+            return render(self.request,self.template_name,{'form':EmployeeEntryForm(), 'entries':entries})
 
 
-    def get_context_data(self, **kwargs):
-        context = super(UserEntryView, self).get_context_data(**kwargs)
-        return context
