4e9f23f4d72e3de7873dc69e0514f9bdc9c529fe
Febry <febry@febry-Satellite-C640.(none)>
2016-02-12 22:23:06 +0530

    expense module completed

--- petroapp/urls.py
+++ petroapp/urls.py
@@ -3,7 +3,6 @@ from petroapp.views import *
 
 urlpatterns = [
     url(r'^entries/$', EntryListView.as_view(), name="entry-list"),
-    #url(r'^(?P<username>\w+)/$',UserEntryView.as_view(), name="user_entry"),
     url(r'^attendance/$', AttendanceCreateView.as_view(), name="attendance_create"),
     url(r'attendance/(?P<pk>[0-9]+)/$', AttendenceClose.as_view(), name='attendence-close'),
     url(r'^add-details/$', EmployeeEntryView.as_view(), name="employee-entry"),
@@ -14,6 +13,8 @@ urlpatterns = [
     url(r'^update-petrol/(?P<pk>[0-9]+)/$', PetroUpdateView.as_view(), name="petro-update"),
     url(r'employees/$', EmployeesListView.as_view(), name="employee_list"),
     url(r'^registration/$', MyRegistrationView.as_view(), name="employee_registration"),
+    url(r'^expenses/list$',ExpenseListView.as_view(), name="expenses_list"),
+    url(r'^expenses/$',ExpenseView.as_view(), name="expenses_record"),
 
 ]
 
--- petroapp/views.py
+++ petroapp/views.py
@@ -184,4 +184,23 @@ class PetroGreenListView(ListView):
 	context['greenarrivals'] = FuelRecords.objects.filter(date__gte=datetime.datetime.now()-timedelta(days=7), fu_type="green")[:10]
 	return context
 
+class ExpenseView(CreateView):
+    model = ExpenseRecord
+    form_class = ExpenseRecordForm
+    template_name = "petroadmin/expense-record.html"
+
+    def form_valid(self,form):
+        expense = form.save(commit=False)
+        expense.save()
+        return HttpResponseRedirect(reverse("expense_list"))
+
+class ExpenseListView(ListView):
+    model = ExpenseRecord
+    template_name = "petroadmin/expense-list.html"
+    def get_context_data(self, *args, **kwargs):
+        context = super(ExpenseListView, self).get_context_data(**kwargs)
+        context['objects_list'] = ExpenseRecord.objects.all()
+        return context
+
+        
 
